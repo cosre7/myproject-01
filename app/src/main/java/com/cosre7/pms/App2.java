b197021cb7fd61442c6586d0e96d8939c1b03a84
cosre7 <cosre7@gmail.com>
2021-01-16 23:41:20 +0900

    v02

---
+++ app/src/main/java/com/cosre7/pms/App2.java
@@ -0,0 +1,15 @@
+package com.cosre7.pms;
+// 신체 지수
+// 1. 날짜
+// 2. 키
+// 3. 몸무게
+// 4. 가슴둘레
+// 5. 허리둘레
+// 6. 허벅지둘레
+// 7. 종아리둘레
+
+public class App2 {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ app/src/main/java/com/cosre7/pms/App3.java
@@ -0,0 +1,12 @@
+package com.cosre7.pms;
+// 운동 일지
+// 1. 날짜
+// 2. 운동 리스트
+// 3. 상체/하체
+// 4. 근력/유산소
+// 5. 운동 강도 (1~5 별)
+public class App3 {
+  public static void main(String[] args) {
+
+  }
+}
