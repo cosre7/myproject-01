706a37f4d72f67061716f1634bca81049ddf3e46
cosre7 <cosre7@gmail.com>
2021-01-20 17:44:24 +0900

    v10

--- app/src/main/java/com/cosre7/pms/DietHandler.java
+++ app/src/main/java/com/cosre7/pms/DietHandler.java
@@ -3,31 +3,37 @@ package com.cosre7.pms;
 import java.sql.Date;
 
 public class DietHandler {
+
+  static class Diet {
+    Date date;
+    String time;
+    String food;
+    int status;
+    int choice;
+  }
   static final int LENGTH  = 100;
-  static Date[] date = new Date[LENGTH];
-  static String[] time = new String[LENGTH];
-  static String[] food = new String[LENGTH];
-  static int[] status = new int[LENGTH];
-  static int[] choice = new int[LENGTH];
+  static Diet[] diets = new Diet[LENGTH];
   static int size = 0;
 
   static void add() {
     System.out.println("[식단일지 작성]");
 
-    date[size] = Prompt.inputDate("날짜 > ");
-    time[size] = Prompt.inputString("시간 > ");
-    food[size] = Prompt.inputString("먹은 음식 > ");
-    status[size] = Prompt.inputInt("포만감 정도 (1~5) > ");     
-    choice[size] = Prompt.inputInt("종류\n1: 식단\n2: 치팅\n> ");
-    size++;
+    Diet d = new Diet();
+    d.date = Prompt.inputDate("날짜 > ");
+    d.time = Prompt.inputString("시간 > ");
+    d.food = Prompt.inputString("먹은 음식 > ");
+    d.status = Prompt.inputInt("포만감 정도 (1~5) > ");     
+    d.choice = Prompt.inputInt("종류\n1: 식단\n2: 치팅\n> ");
+    diets[size++] = d;
   }
 
   static void list() {
     System.out.println("[식단일지 목록]");
 
     for(int i = 0; i < size; i++) {
+      Diet d = diets[i];
       String label = null;
-      switch (choice[i]) {
+      switch (d.choice) {
         case 1:
           label = "훌륭해요!";
           break;
@@ -36,7 +42,7 @@ public class DietHandler {
           break;
       }
       String statusLabel = null;
-      switch (status[i]) {
+      switch (d.status) {
         case 1:
           statusLabel = "나는 아직 배고프다..";
           break;
@@ -53,7 +59,7 @@ public class DietHandler {
           statusLabel = "터질 것 같아요";
       }
       System.out.printf("날짜: %s\n시간: %s시\n먹은 음식: %s\n포만감: %s\n결론: %s\n", 
-          date[i], time[i], food[i], statusLabel, label);
+          d.date, d.time, d.food, statusLabel, label);
     }
   }
 
--- app/src/main/java/com/cosre7/pms/TrainingHandler.java
+++ app/src/main/java/com/cosre7/pms/TrainingHandler.java
@@ -3,33 +3,41 @@ package com.cosre7.pms;
 import java.sql.Date;
 
 public class TrainingHandler {
-  static final int LENGTH3 = 100;
-  static Date[] date2 = new Date[LENGTH3];
-  static String[] list = new String[LENGTH3];
-  static int[] body = new int[LENGTH3];
-  static int[] training = new int[LENGTH3];
-  static int[] intensity = new int[LENGTH3];
-  static double[] runTime = new double[LENGTH3];
-  static int size3 = 0;
+
+  static class Training {
+    Date date;
+    String list;
+    int body;
+    int training;
+    int intensity;
+    double runTime;
+  }
+  static final int LENGTH = 100;
+  static Training[] trainings = new Training[LENGTH];
+  static int size = 0;
 
   static void add() {
     System.out.println("[운동일지 작성]");
 
-    date2[size3] = Prompt.inputDate("날짜 > ");
-    list[size3] = Prompt.inputString("운동 리스트 > ");
-    body[size3] = Prompt.inputInt("종류1\n1: 상체\n2: 하체\n3: 전신\n> ");
-    training[size3] = Prompt.inputInt("종류2\n1: 근력\n2: 유산소\n> ");
-    runTime[size3] = Prompt.inputDouble("소요시간 > ");
-    intensity[size3] = Prompt.inputInt("운동 강도 (1~5) > ");
-    size3++;
+    Training t = new Training();
+
+    t.date = Prompt.inputDate("날짜 > ");
+    t.list = Prompt.inputString("운동 리스트 > ");
+    t.body = Prompt.inputInt("종류1\n1: 상체\n2: 하체\n3: 전신\n> ");
+    t.training = Prompt.inputInt("종류2\n1: 근력\n2: 유산소\n> ");
+    t.runTime = Prompt.inputDouble("소요시간 > ");
+    t.intensity = Prompt.inputInt("운동 강도 (1~5) > ");
+
+    trainings[size++] = t;
   }
 
   static void list() {
     System.out.println("[운동일지 목록]");
 
-    for (int i = 0; i < size3; i++) {
+    for (int i = 0; i < size; i++) {
+      Training t = trainings[i];
       String bodyLabel = null;
-      switch (body[i]) {
+      switch (t.body) {
         case 1:
           bodyLabel = "상체 운동";
           break;
@@ -40,7 +48,7 @@ public class TrainingHandler {
           bodyLabel = "전신 운동";
       }
       String trainingLabel = null;
-      switch (training[i]) {
+      switch (t.training) {
         case 1:
           trainingLabel = "근력 운동";
           break;
@@ -49,7 +57,7 @@ public class TrainingHandler {
           break;
       }
       String intensityLabel = null;
-      switch (intensity[i]) {
+      switch (t.intensity) {
         case 1:
           intensityLabel = "껌이에요";
           break;
@@ -66,7 +74,7 @@ public class TrainingHandler {
           intensityLabel = "헬이에요";
       }
       System.out.printf("날짜: %s\n운동 목록: %s\n종류1: %s\n종류2: %s\n소요 시간: %f\n운동 강도: %s\n", 
-          date2[i], list[i], bodyLabel, trainingLabel, runTime[i], intensityLabel);
+          t.date, t.list, bodyLabel, trainingLabel, t.runTime, intensityLabel);
     }
   }
 
