671f7d41ce8e056657eb129573ec4fe71fbad021
cosre7 <cosre7@gmail.com>
2021-01-24 22:28:12 +0900

    v14-b

--- app/src/main/java/com/cosre7/pms/handler/BoardHandler.java
+++ app/src/main/java/com/cosre7/pms/handler/BoardHandler.java
@@ -1,5 +1,6 @@
 package com.cosre7.pms.handler;
 
+import java.sql.Date;
 import com.cosre7.util.Prompt;
 
 public class BoardHandler {
@@ -8,6 +9,8 @@ public class BoardHandler {
     String title;
     String content;
     String writer;
+    Date registeredDate;
+    int viewCount;
   }
 
   static final int LENGTH = 100;
@@ -22,9 +25,19 @@ public class BoardHandler {
     b.no = Prompt.inputInt("번호 > ");
     b.title = Prompt.inputString("제목 > ");
     b.writer = Prompt.inputString("작성자 > ");
+    b.registeredDate = new Date(System.currentTimeMillis());
 
     boards[size++] = b;
   }
 
+  public static void list() {
+    System.out.println("[게시글 목록]");
+
+    for (int i = 0; i < size; i++) {
+      Board b = boards[i];
+      System.out.printf("번호: %d\n제목: %s\n작성자: %s\n등록일: %s\n조회수: %d",
+          b.no, b.title, b.writer, b.registeredDate, b.viewCount);
+    }
+  }
 
 }
