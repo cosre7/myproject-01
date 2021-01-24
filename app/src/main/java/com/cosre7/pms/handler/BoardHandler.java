acf66760e82d26361b24e2f1f87a3a9c7b793518
cosre7 <cosre7@gmail.com>
2021-01-24 22:18:56 +0900

    v14-a

---
+++ app/src/main/java/com/cosre7/pms/handler/BoardHandler.java
@@ -0,0 +1,28 @@
+package com.cosre7.pms.handler;
+
+import com.cosre7.util.Prompt;
+
+public class BoardHandler {
+  static class Board {
+    int no;
+    String title;
+    String content;
+    String writer;
+  }
+
+  static final int LENGTH = 100;
+  static Board[] boards = new Board[LENGTH];
+  static int size = 0;
+
+  public static void add() {
+    System.out.println("[게시글 작성]");
+
+    Board b = new Board();
+
+    b.no = Prompt.inputInt("번호 > ");
+    b.title = Prompt.inputString("제목 > ");
+    b.writer = Prompt.inputString("작성자 > ");
+
+    boards[size++] = b;
+  }
+}
