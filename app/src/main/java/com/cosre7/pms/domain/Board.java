123450143e353f43e453de245da0831a5468e168
cosre7 <cosre7@gmail.com>
2021-01-24 22:34:52 +0900

    v14-c

---
+++ app/src/main/java/com/cosre7/pms/domain/Board.java
@@ -0,0 +1,13 @@
+package com.cosre7.pms.domain;
+
+import java.sql.Date;
+
+public class Board {
+  public int no;
+  public String title;
+  public String content;
+  public String writer;
+  public Date registeredDate;
+  public int viewCount;
+
+}
--- app/src/main/java/com/cosre7/pms/handler/BoardHandler.java
+++ app/src/main/java/com/cosre7/pms/handler/BoardHandler.java
@@ -1,23 +1,16 @@
 package com.cosre7.pms.handler;
 
 import java.sql.Date;
+import com.cosre7.pms.domain.Board;
 import com.cosre7.util.Prompt;
 
 public class BoardHandler {
-  static class Board {
-    int no;
-    String title;
-    String content;
-    String writer;
-    Date registeredDate;
-    int viewCount;
-  }
 
   static final int LENGTH = 100;
-  static Board[] boards = new Board[LENGTH];
-  static int size = 0;
+  Board[] boards = new Board[LENGTH];
+  int size = 0;
 
-  public static void add() {
+  public void add() {
     System.out.println("[게시글 작성]");
 
     Board b = new Board();
@@ -27,14 +20,14 @@ public class BoardHandler {
     b.writer = Prompt.inputString("작성자 > ");
     b.registeredDate = new Date(System.currentTimeMillis());
 
-    boards[size++] = b;
+    this.boards[this.size++] = b;
   }
 
-  public static void list() {
+  public void list() {
     System.out.println("[게시글 목록]");
 
-    for (int i = 0; i < size; i++) {
-      Board b = boards[i];
+    for (int i = 0; i < this.size; i++) {
+      Board b = this.boards[i];
       System.out.printf("번호: %d\n제목: %s\n작성자: %s\n등록일: %s\n조회수: %d",
           b.no, b.title, b.writer, b.registeredDate, b.viewCount);
     }
