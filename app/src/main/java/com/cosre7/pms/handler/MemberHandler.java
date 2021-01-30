edd651ae2bd7d2050156b400faa71b4b389644d7
cosre7 <cosre7@gmail.com>
2021-01-30 22:15:56 +0900

    .

--- app/src/main/java/com/cosre7/pms/handler/MemberHandler.java
+++ app/src/main/java/com/cosre7/pms/handler/MemberHandler.java
@@ -4,34 +4,43 @@ import java.sql.Date;
 import com.cosre7.util.Prompt;
 
 public class MemberHandler {
-  static final int LENGTH = 100;
-  static int[] no = new int[LENGTH];
-  static String[] name = new String[LENGTH];
-  static String[] password = new String[LENGTH];
-  static String[] photo = new String[LENGTH];
-  static String[] tel = new String[LENGTH];
-  static Date[] registeredDate = new Date[LENGTH];
-  static int size = 0;
 
-  static void add() {
-    System.out.println("[회원 등록]");
-
-    no[size] = Prompt.inputInt("번호? ");
-    name[size] = Prompt.inputString("이름? ");
-    password[size] = Prompt.inputString("암호? ");
-    photo[size] = Prompt.inputString("사진? ");
-    tel[size] = Prompt.inputString("전화? ");
-    registeredDate[size] = new Date(System.currentTimeMillis());
-    size++;
+  static class Member {
+    int no;
+    String name;
+    String password;
+    String photo;
+    String tel;
+    Date registeredDate;
   }
 
-  static void list() {
+  static final int LENGTH = 100;
+  static Member[] members = new Member[LENGTH];
+  static int size = 0;
+
+  public static void add() {
+    System.out.println("[회원 등록]");
+
+    Member m = new Member();
+
+    m.no = Prompt.inputInt("번호? ");
+    m.name = Prompt.inputString("이름? ");
+    m.password = Prompt.inputString("암호? ");
+    m.photo = Prompt.inputString("사진? ");
+    m.tel = Prompt.inputString("전화? ");
+    m.registeredDate = new Date(System.currentTimeMillis());
+
+    members[size++] = m;
+  }
+
+  public static void list() {
     System.out.println("[회원 목록]");
 
     //번호, 이름, 전화, 가입일
     for (int i = 0; i < size; i++) {
+      Member m = members[i];
       System.out.printf("%d, %s, %s, %s\n",
-          no[i], name[i], tel[i], registeredDate[i]);
+          m.no, m.name, m.tel, m.registeredDate);
     }
   }
 }
