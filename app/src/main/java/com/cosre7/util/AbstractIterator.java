ea681ff1e99521c0cf9b8f6c035b9be3247e425e
cosre7 <cosre7@gmail.com>
2021-02-15 17:47:10 +0900

    20-c-1

--- app/src/main/java/com/cosre7/util/AbstractIterator.java
+++
@@ -1,6 +0,0 @@
-package com.cosre7.util;
-
-public abstract class AbstractIterator {
-  public abstract boolean hasNext();
-  public abstract Object next();
-}
---
+++ app/src/main/java/com/cosre7/util/Iterator.java
@@ -0,0 +1,6 @@
+package com.cosre7.util;
+
+public interface Iterator {
+  boolean hasNext();
+  Object next();
+}
