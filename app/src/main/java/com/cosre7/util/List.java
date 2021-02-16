133600c0a11b5e1a95a3f1506e08ca02c9cc5ded
cosre7 <cosre7@gmail.com>
2021-02-16 18:06:05 +0900

    20-e-1

--- app/src/main/java/com/cosre7/util/List.java
+++ app/src/main/java/com/cosre7/util/List.java
@@ -138,4 +138,23 @@ public class List {
   public Iterator iterator() throws CloneNotSupportedException {
     return new ListIterator(this);
   }
+
+  private static class ListIterator implements Iterator {
+    List list;
+    int cursor = 0;
+
+    public ListIterator(List list) {
+      this.list = list;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return cursor < list.size();
+    }
+
+    @Override
+    public Object next() {
+      return list.get(cursor++);
+    }
+  }
 }
